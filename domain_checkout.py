97aef793e456331c66286183e50707979b05830a
karmazynowyksiaze <ppawlowski186@gmail.com>
2025-06-16 17:11:55 +0200

    add read_file func

--- domain_checkout.py
+++ domain_checkout.py
@@ -1,1 +1,28 @@
-print (f"Hello World!")
+import sys
+import time
+from urllib.parse import urlparse
+
+def read_domains_from_file(filename):
+    """READ DOMAIN FROM TEXT FILE FUNCTION"""
+    domains = []
+    try:
+        with open(filename, 'r', encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    if line.startswith(('http://', 'https://')):
+                        domain = urlparse(line).netloc
+                    else:
+                        domain = line
+                    domains.append(domain)
+    except FileNotFoundError:
+        print(f"[ERR]: File {filename} not found!")
+        sys.exit(1)
+    except Exception as e:
+        print(f"[ERR]: Error while reading file: {e}")
+        sys.exit(1)
+    
+    return domains
+
+
+    
